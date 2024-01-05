d8480d061dde08d96df9f4c84108770c8b1ecef7
KateM3d <ekaterina8medvedeva@gmail.com>
2024-01-05 12:03:42 -0500

    add register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,14 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegisterUser = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    axios.get("http://localhost:4000/test");
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@emailcom" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={handleRegisterUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@emailcom"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
             Already a member?{" "}
